28fa0559fd960564de99a93c7e18c40399ba5b2a
lttboy25 <139521084+lttboy25@users.noreply.github.com>
2023-10-11 09:04:54 +0700

    T5_4.cpp

--- T5_4.cpp
+++ T5_4.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
 int main() {
-	int n=-20,uoc;
-	for (uoc=1;uoc<=n;uoc++) {
-		if (n%uoc==0) 
-		{ printf("%d,",uoc);}
-		
-		
-	}
-	
-	return 0;
+    int n;
+    scanf("%d",&n);
+    
+    for (int i=1;i<=n;i++) {
+        if (n%i==0) {
+            printf("%d",i);
+		// create space if i==n space stop program
+            if (i != n) { 
+                printf(", ");
+            }
+        }
+        
+    }
+    
+    return 0;
 }
